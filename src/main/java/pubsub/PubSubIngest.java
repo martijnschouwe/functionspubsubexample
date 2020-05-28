ebd4c1a78c7c7a8664ff2ec01ead5058d5eda8bd
martijnschouwe <martijnschouwe@hotmail.com>
2020-05-28 16:31:15 +0200

    Fine tuning

--- src/main/java/pubsub/PubSubIngest.java
+++ src/main/java/pubsub/PubSubIngest.java
@@ -1,4 +1,29 @@
 package pubsub;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.function.Consumer;
+
+@SpringBootApplication
 public class PubSubIngest {
+    private static final Log log = LogFactory.getLog(PubSubIngest.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubSubIngest.class, args);
+    }
+
+    @Bean
+    public Consumer<PubSubMessage> ingest() {
+        return message -> {
+            // The PubSubMessage data field is base-64 encoded which must be decoded.
+            String decodedMessage = new String(Base64.getDecoder().decode(message.getData()), StandardCharsets.UTF_8);
+            System.out.println("Received Pub/Sub message with data: " + decodedMessage);
+        };
+    }
 }
--- src/test/java/pubsub/PubSubIngestTest.java
+++ src/test/java/pubsub/PubSubIngestTest.java
@@ -1,4 +1,19 @@
 package pubsub;
 
+import org.junit.Test;
+
+import java.util.function.Consumer;
+
 public class PubSubIngestTest {
+
+    Consumer<PubSubMessage> function;
+
+    @Test
+    public void testFunction() throws Exception {
+        PubSubMessage p = new PubSubMessage();
+        p.setData("TEST");
+        function = new PubSubIngest().ingest();
+        this.function.accept(p);
+    }
+    
 }
